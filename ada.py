622f28cd397f2a73238c560af06a0a238efd8499
iniakidominguez <73805462+iniakidominguez@users.noreply.github.com>
2025-03-31 12:06:11 -0300

    Update ada.py

--- ada.py
+++ ada.py
@@ -1,8 +1,8 @@
-def ada():
+    def ada():
     first_name = "AdA"
     last_name = "LoVeLAce"
     Full_Name= f""{fisrt_name}{Last_name}
-    print (full_name.lower())
-    print (full_name.title())
-    print (full_name.upper())
-    print (f'\t{full_name.lower()}')
+    print (Full_name.lower())
+    print (Full_name.title())
+    print (Full_name.upper())
+    print (f'\t{Full_name.lower()}')
